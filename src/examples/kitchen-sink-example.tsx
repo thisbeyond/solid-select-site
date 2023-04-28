d9858d1e9ec704babd55ef059e0476e2696b8e8b
Martin Pengelly-Phillips <dev@thisbeyond.com>
2023-04-28 20:00:45 +0100

    Add quick pick to kitchen sink example.

--- src/examples/kitchen-sink-example.tsx
+++ src/examples/kitchen-sink-example.tsx
@@ -1,4 +1,4 @@
-import { createSignal, createUniqueId } from "solid-js";
+import { For, createSignal, createUniqueId } from "solid-js";
 
 import { Select, createOptions } from "@thisbeyond/solid-select";
 
@@ -17,19 +17,19 @@ export const KitchenSinkExample = () => {
 
   const initialValue = [candidates[2]];
 
-  const [values, setValues] = createSignal(candidates);
+  const [options, setOptions] = createSignal(candidates);
   const [selectedValues, setSelectedValues] = createSignal(initialValue);
 
   const onChange = (selected) => {
     setSelectedValues(selected);
 
     const lastValue = selected[selected.length - 1];
-    if (lastValue && !values().includes(lastValue)) {
-      setValues([...values(), lastValue]);
+    if (lastValue && !options().includes(lastValue)) {
+      setOptions([...options(), lastValue]);
     }
   };
 
-  const props = createOptions(values, {
+  const props = createOptions(options, {
     key: "name",
     disable: (value) => selectedValues().includes(value),
     filterable: true, // Default
@@ -37,11 +37,31 @@ export const KitchenSinkExample = () => {
   });
 
   return (
-    <Select
-      multiple
-      initialValue={initialValue}
-      onChange={onChange}
-      {...props}
-    />
+    <div class="flex flex-1 flex-col max-w-100 gap-3 bg-transparent">
+      <Select
+        class="bg-white"
+        multiple
+        initialValue={selectedValues()}
+        onChange={onChange}
+        {...props}
+      />
+      <div class="flex gap-3 text-sm items-center">
+        Quick pick:
+        <For
+          each={options()
+            .filter((option) => !selectedValues().includes(option))
+            .slice(0, 3)}
+        >
+          {(option) => (
+            <button
+              class="bg-blue-500 text-white px-2 py-1 rounded"
+              onClick={() => setSelectedValues([...selectedValues(), option])}
+            >
+              {option.name}
+            </button>
+          )}
+        </For>
+      </div>
+    </div>
   );
 };
